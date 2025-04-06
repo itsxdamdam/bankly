438df911d6aae32419d64b3e8744fd792d82b830
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-04-06 13:27:29 +0100

    update data with data from db

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -73,6 +73,7 @@ export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
     const accountsResponse = await plaidClient.accountsGet({
       access_token: bank.accessToken,
     });
+
     const accountData = accountsResponse.data.accounts[0];
 
     // get transfer transactions from appwrite
@@ -115,7 +116,7 @@ export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
     };
 
     // sort transactions by date such that the most recent transaction is first
-      const allTransactions = [...transactions, ...transferTransactions].sort(
+    const allTransactions = [...transactions, ...transferTransactions].sort(
       (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
     );
 
