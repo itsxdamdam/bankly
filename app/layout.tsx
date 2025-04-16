47c4af1d3dad409bd16d3129700923de771be031
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-04-16 01:49:22 +0100

    add-force dynamic and global-error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-inter"
+  variable: "--font-inter",
 });
 
 const ibmPlexSerif = IBM_Plex_Serif({
   variable: "--font-ibm-plex-serif",
   subsets: ["latin"],
-  weight: ["400", "700"]
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
   title: "Horizbank",
   description: "Modern banking platform",
   icons: {
-    icon: "/icons/logo.svg"
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
